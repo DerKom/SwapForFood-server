fa0cf69f153444754c61c5a2a50f77e478b2a24f
DerKom <DerKom@users.noreply.github.com>
2024-12-19 13:34:54 +0000

    fixed leak

--- SwapForFood_server/utils/restaurant_fetcher.py
+++ SwapForFood_server/utils/restaurant_fetcher.py
@@ -4,7 +4,7 @@ import math
 from models.restaurant import Restaurant
 
 # Cargar la API Key desde variable de entorno
-API_KEY = "AIzaSyAXxCIuMUbUcGSy6pstHOE1YEKPt6dLpqY"
+API_KEY = "Aqui tu key"
 PLACE_TYPE = "restaurant"
 MAX_RESULTS = 2
 
